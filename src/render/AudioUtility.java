dc6651389be6e25eba5a75ab2b1dabe928b2f8f4
5730653821-SV <suwapat.wow@gmail.com>
2015-12-10 18:35:17 +0700

    init

---
+++ src/render/AudioUtility.java
@@ -0,0 +1,17 @@
+package render;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+public class AudioUtility {
+	private static AudioClip acIntro;
+	static{
+		ClassLoader loader = AudioUtility.class.getClassLoader();
+		acIntro = Applet.newAudioClip(loader.getResource("res/intro.wav"));
+	}
+	public static void playSound(String identifier){
+		if(identifier.equals("intro")) acIntro.play();
+		
+	}
+
+}
