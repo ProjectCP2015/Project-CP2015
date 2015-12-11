fe88c35f3bc09046668136d8f50fbabe82ebe28a
INTEL <INTEL@ASUS>
2015-12-12 02:28:12 +0700

    nothing much

---
+++ src/beatmap/Song.java
@@ -0,0 +1,11 @@
+package beatmap;
+
+import java.awt.image.BufferedImage;
+
+public abstract class Song {
+	protected String name;
+	protected int bpm;
+	protected BufferedImage pic;
+	
+
+}
--- src/ui/MenuScreen.java
+++ src/ui/MenuScreen.java
@@ -1,7 +1,18 @@
 package ui;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridBagLayout;
+
 import javax.swing.JPanel;
 
 public class MenuScreen extends JPanel {
-	
+	public MenuScreen(){
+		super();
+		//setPreferredSize(new Dimension(, arg1));
+		setLayout(new GridBagLayout());
+	}
+	public void paintComponent(Graphics g){
+		
+	}
 }
