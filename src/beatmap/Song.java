2cfa9eb8ec36022838cf3ddb30d0189b47f1a94c
INTEL <INTEL@ASUS>
2015-12-13 17:04:52 +0700

    ft.Print

--- src/beatmap/Song.java
+++ src/beatmap/Song.java
@@ -1,11 +1,17 @@
 package beatmap;
 
+import java.applet.AudioClip;
 import java.awt.image.BufferedImage;
 
-public abstract class Song {
-	protected String name;
-	protected int bpm;
-	protected BufferedImage pic;
-	protected int highScore, highCombo;
+public class Song {
+	private String name;
+	private  BufferedImage pi;
+	private AudioClip deemo;
+	private int highScore, highCombo;
+	 
+	
+	public Song(String url){
+		
+	}
 
 }
